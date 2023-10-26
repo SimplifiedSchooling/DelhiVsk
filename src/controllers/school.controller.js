38f24a828436c4f2ae2148000c5f471d7cc8a913
suhas <suhas@simplifiedschooling.com>
2023-10-26 11:05:22 +0530

    school bulkupload

--- src/controllers/school.controller.js
+++ src/controllers/school.controller.js
@@ -1,6 +1,30 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { schoolService } = require('../services');
+const { join } = require('path');
+const csv = require('csvtojson');
+const ApiError = require('../utils/ApiError');
+
+const staticFolder = join(__dirname, '../');
+const uploadsFolder = join(staticFolder, 'uploads');
+
+const bulkUploadFile = catchAsync(async (req, res) => {
+ 
+  if (req.file) {
+    if (req.file.mimetype !== 'text/csv') {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Uploaded file must be in CSV format.');
+    }
+
+    const csvFilePath = join(uploadsFolder, req.file.filename);
+    const csvJsonArray = await csv().fromFile(csvFilePath);
+    const result = await schoolService.bulkUpload(csvJsonArray);
+
+    res.status(httpStatus.CREATED).json(result);
+  } else {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Missing file');
+  }
+});
+
 
 const storeSchoolDataInMongoDB = catchAsync(async (req, res) => {
   const result = await schoolService.storeSchoolDataInMongoDB();
@@ -14,4 +38,5 @@ const schoolData = catchAsync(async (req, res) => {
 module.exports = {
   storeSchoolDataInMongoDB,
   schoolData,
+  bulkUploadFile,
 };
