38f8389fb6941d667b824fcae00b12d2200fcc5e
suhas <suhas@simplifiedschooling.com>
2023-10-31 14:56:32 +0530

    changes

--- src/config/config.js
+++ src/config/config.js
@@ -23,6 +23,8 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    REDIS_PORT: Joi.string(),
+    REDIS_HOST: Joi.string(),
   })
   .unknown();
 
@@ -60,5 +62,9 @@ module.exports = {
       },
     },
     from: envVars.EMAIL_FROM,
+  },
+  redis: {
+    host: envVars.REDIS_HOST,
+    port: envVars.REDIS_PORT,
   },
 };
