20b082c1e89ef2cb07dc3e5bb535be3fd4b0f2fa
suhas <suhas@simplifiedschooling.com>
2024-01-10 14:25:54 +0530

    tabular view attendance

--- src/routes/v1/tabular.attendance.route.js
+++ src/routes/v1/tabular.attendance.route.js
@@ -4,6 +4,9 @@ const { tabularAttendaceController } = require('../../controllers');
 const router = express.Router();
 
 router.route('/').post(tabularAttendaceController.getAttendanceData);
+router.route('/get-district').get(tabularAttendaceController.getAllDistrictsAndZones);
+
+router.route('/get-studnet-health').get(tabularAttendaceController.getStudentHealth);
 module.exports = router;
 
 /**
@@ -37,6 +40,44 @@ module.exports = router;
  *                 type: string
  *             required:
  *               - attendance_DATE
+ *     responses:
+ *       "200":
+ *         description: OK
+ *       "401":
+ *         description: Unauthorized
+ *       "403":
+ *         description: Forbidden
+ */
+
+/**
+ * @swagger
+ * /tabular-attendnace/get-district:
+ *   get:
+ *     summary: Get all district and zone.
+ *     description: Get all district and zone.
+ *     tags: [Tabular-Attendnace]
+ *     responses:
+ *       "200":
+ *         description: OK
+ *       "401":
+ *         description: Unauthorized
+ *       "403":
+ *         description: Forbidden
+ */
+
+/**
+ * @swagger
+ * /tabular-attendnace/get-studnet-health:
+ *   post:
+ *     summary: Get tabular attendance.
+ *     description: Get tabular attendance.
+ *     tags: [Tabular-Attendnace]
+ *     query:
+ *             properties:
+ *               Schoolid:
+ *                 type: string
+ *             required:
+ *               - Schoolid
  *     responses:
  *       "200":
  *         description: OK
