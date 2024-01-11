9c9687c27349bbf0e690bf9dfa2f2fa7f6400b78
suhas <suhas@simplifiedschooling.com>
2024-01-11 14:21:15 +0530

    map

--- src/controllers/tabular.attendance.controller.js
+++ src/controllers/tabular.attendance.controller.js
@@ -1,13 +1,14 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
+const pick = require('../utils/pick');
 const { tabularAttendanceService } = require('../services');
 
 const getAttendanceData = catchAsync(async (req, res) => {
-  const {Z_name, School_ID, shift,attendance_DATE} = req.body;
+  const {Z_name, School_ID, shift,attendance_DATE,district_name } = req.body;
   if(!attendance_DATE){
       return res.status(httpStatus.BAD_REQUEST).json({ error: 'Missing parameter date' });
   }
-const result = await tabularAttendanceService.getAttendanceData(Z_name, School_ID, shift,attendance_DATE);
+const result = await tabularAttendanceService.getAttendanceData(Z_name, School_ID, shift,attendance_DATE, district_name);
   res.status(httpStatus.CREATED).send(result);
 });
 
@@ -19,13 +20,22 @@ if(!result){
   res.status(httpStatus.CREATED).send(result);
 });
 
-const getStudentHealth= catchAsync(async (req, res) => {
-  const result = await tabularAttendanceService.studentHealth(req.query.Schoolid);
-  if(!result){
-    return res.status(httpStatus.NOT_FOUND).json(  'Data not found' );
-  }
-    res.status(httpStatus.CREATED).send(result);
+// const getStudentHealth= catchAsync(async (req, res) => {
+//   const result = await tabularAttendanceService.studentHealth(req.query.Schoolid);
+//   if(!result){
+//     return res.status(httpStatus.NOT_FOUND).json(  'Data not found' );
+//   }
+//     res.status(httpStatus.CREATED).send(result);
+//   });
+
+  const getStudentHealth = catchAsync(async (req, res) => {
+    const filter = pick(req.query, ['Schoolid, Name, S_ID']);
+    const options = pick(req.query, ['sortBy', 'limit', 'page']);
+    options.sortBy = 'CLASS';
+    const allClasses = await tabularAttendanceService.studentHealth(filter, options);
+    res.send(allClasses);
   });
+  
 module.exports = {
     getAttendanceData,
     getAllDistrictsAndZones,
